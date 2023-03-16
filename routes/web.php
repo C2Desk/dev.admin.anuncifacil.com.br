c17b9cf38ee163adf1b1a90777506ecb59f77060
Luiz Tinti <79257881+firestormDev@users.noreply.github.com>
2023-03-16 15:22:53 -0300

    Fix ambiente
    Fix ambiente

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\PostsController;
-use App\Http\Controllers\PubController;
-use App\Http\Controllers\AccountController;
-use App\Http\Controllers\UsersController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +14,9 @@ use App\Http\Controllers\UsersController;
 |
 */
 
-Route::get('/',[DashController::class,'index']) ->middleware('auth');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -30,49 +27,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-
-Route::prefix('posts')->middleware('auth')->group(function ()
-{
-    Route::get('/', [PostsController::class, 'index'])->name('posts.index');
-    Route::get('/list', [PostsController::class, 'paginacao'])->name('posts.list');
-
-    Route::get('/create', [PostsController::class, 'create'])->name('posts.create');
-    Route::get('/social', [PostsController::class, 'social'])->name('posts.social');
-
-    Route::post('/save', [PostsController::class, 'store'])->name('posts.save');
-
-    /*UPDATE*/
-    Route::get('/edit/{id}', [PostsController::class, 'edit'])->name('posts.edit');
-    Route::post('/update', [PostsController::class, 'update'])->name('posts.update');
-    Route::put('/destaque', [PostsController::class, 'updateDestaque'])->name('posts.destaque');
-
-
-    Route::delete('/delete/{id}',[PostsController::class, 'destroy'])->name('posts.destroy');
-});
-
-
-
-//Publicidade
-
-Route::get('/publicidade', [PubController::class, 'index']);
-Route::get('/publicidade/list', [PubController::class, 'paginacao'])->name('publicidade.list');
-
-Route::get('/publicidade/create', [PubController::class, 'create']);
-Route::get('/publicidade/pdf', [PubController::class, 'pdf'])->name('publicidade.pdf');
-Route::post('/publicidade/save', [PubController::class, 'store'])->name('publicidade.save');
-
-Route::get('/publicidade/edit/{id}', [PubController::class, 'edit'])->name('publicidade.edit');
-Route::put('/publicidade/{id}', [PubController::class, 'update'])->name('publicidade.update');
-
-Route::delete('/publicidade/delete/{id}',[PubController::class, 'destroy'])->name('publicidade.destroy');
-
-
-Route::post('/ckeditor',[PubController::class, 'uploadimage'])->name('ckeditor.upload');
-
-//Accouts
-Route::get('/account/profile',[AccountController::class, 'profile'])->name('account.profile');
-Route::get('/account/users',[UsersController::class, 'index'])->name('account.users');
-Route::get('/account/list', [AccountController::class, 'paginacao'])->name('account.list');
 
 require __DIR__.'/auth.php';
