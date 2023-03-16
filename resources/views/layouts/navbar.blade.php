7ae58d28137765c7e0ec7bff2a7187df394b0bb7
Luiz Tinti <79257881+firestormDev@users.noreply.github.com>
2023-03-16 15:49:38 -0300

    fix logout
    fix logout

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -74,10 +74,17 @@
                                         <div class="dropdown-divider"></div>
                                     </li>
                                     <li>
-                                        <a class="dropdown-item" href="auth-login-basic.html">
-                                            <i class="bx bx-power-off me-2"></i>
-                                            <span class="align-middle">Log Out</span>
-                                        </a>
+                                        <form method="POST" action="{{ route('logout') }}">
+                                            @csrf
+                                            <x-dropdown-link class="dropdown-item" :href="route('logout')"
+                                                onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                                <i class="bx bx-power-off me-2"></i>
+                                                <span class="align-middle">Log Out</span>
+                                            </x-dropdown-link>
+                                        </form>
+
+
                                     </li>
                                 </ul>
                             </li>
